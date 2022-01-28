ea54537b52807378599556e1c37243601fce2784
jeondui <jeondui@gmail.com>
2022-01-28 14:52:20 +0900

    member service development

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 회원 repository interface
+ */
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findByID(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
