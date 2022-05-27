71216ed8226afda0695d34b06b7571c48b7563af
jeondui <jeondui@gmail.com>
2022-05-27 20:04:22 +0900

    Spring Container Update

--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -7,27 +7,35 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다. (= 기획자)
  * 객체를 생성하고 연결하는 역할과 실행하는 역할이 명확히 분리되었다. (관심사의 분리)
  */
-public class AppConfig {
 
+
+@Configuration  // 애플리케이션의 설정정보를 담당
+public class AppConfig {
     // MemberServiceImpl의 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부인 AppConfig에서 결정된다.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자를 통해서 객체를 주입(연결) -> DI(Dependency Injection)
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 마찬가지로 OrderServiceImpl의 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부인 AppConfig에서 결정된다.
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy()); // 생성자를 통해서 객체를 주입(연결) -> DI(Dependency Injection)
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
 //        return new RateDiscountPolicy();  // 할인 정책 변경 시 이 코드만 수정하면 바로 적용 가능
--- core/src/main/java/hello/core/order/OrderApp.java
+++ core/src/main/java/hello/core/order/OrderApp.java
@@ -4,12 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        // ApplicationContext는 스프링 컨테이너라고 한다.
+        // 기존에는 AppConfig 클래스를 사용해서 직접 객체를 생성하고 DI했지만, 이제부터는 스프링 컨테이너를 사용한다.
+        // 스프링 컨테이너는 @Configuration이 붙은 AppConfig를 구성 정보로 사용한다.
+        // @Bean이라고 적힌 메소드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다. 이처럼 스프링 컨테이너에 등록된 객체를 스프링 빈이라고 한다.
+        // 스프링 컨테이너에게 객체를 스프링 빈으로 등록하고, 스프링 컨테이너에서 스프링 빈을 찾아서 사용한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
