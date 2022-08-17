c60322ba947d9994b2d17f23cfa97d98b8c9a57c
jeondui <jeondui@gmail.com>
2022-08-17 22:53:39 +0900

    commit

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,17 +10,21 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
+    /**
+     * HTTP 요청을 통해 매핑된 URL이 호출되면 서블릿 컨테이너는 service 메서드를 실행함
+     */
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
 
-        String username = request.getParameter("username");
+        String username = request.getParameter("username"); // 쿼리 파라미터 값
         System.out.println("username = " + username);
 
+        // 응답 메시지
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello" + username);
+        response.getWriter().write("hello" + username); // Http body부분에 내용이 들어감
     }
 }
